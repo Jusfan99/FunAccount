1b3891274d9dd5d40d22b61f5e453cf32936466b
Jusfan <jiasifan@kuaishou.com>
2020-12-14 17:38:02 +0800

    修改Recyclerview的写法 将数据初始化和适配器绑定等一系列操作放在SettingItemFragment中进行 并将其作为SettingFragment的父类 避免SettingFragment中代码冗余

--- app/src/main/java/com/example/funaccount/setting_page/SettingFragment.java
+++ app/src/main/java/com/example/funaccount/setting_page/SettingFragment.java
@@ -1,63 +1,22 @@
 package com.example.funaccount.setting_page;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import com.example.funaccount.R;
-import com.example.funaccount.util.SettingItem;
-import com.example.funaccount.util.SettingItemAdapter;
-
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.example.funaccount.util.SettingItemFragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.ListFragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-public class SettingFragment extends Fragment {
-
-    private String[] title = {"预算中心","高级功能","其他设置","账号设置","常见问题","好评鼓励","关于我们"};
-    private int[] images = {R.drawable.setting4,R.drawable.setting2,R.drawable.setting1,R.drawable.setting3,R.drawable.setting5,R.drawable.setting6,R.drawable.setting7};
-    private RecyclerView recyclerView;
-    private SettingItemAdapter settingItemAdapter;
-    private ArrayList<SettingItem> settingItems = new ArrayList<SettingItem>();
-
-    public SettingFragment() {
-    }
-
+public class SettingFragment extends SettingItemFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.setting_frag,container,false);
         initRecyclerView(view);
-        initData();
         return view;
-    }
-
-    private void initRecyclerView(View view){
-        recyclerView = view.findViewById(R.id.setting_recycleview);
-        settingItemAdapter = new SettingItemAdapter(getActivity(),settingItems);
-        recyclerView.setAdapter(settingItemAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        //待更改
-    }
-    private void initData(){
-        for(int i = 0; i < 7; i++){
-            SettingItem item = new SettingItem(images[i],title[i]);
-            settingItems.add(item);
-        }
     }
 }
---
+++ app/src/main/java/com/example/funaccount/util/SettingItemFragment.java
@@ -0,0 +1,105 @@
+package com.example.funaccount.util;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.funaccount.R;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SettingItemFragment extends Fragment {
+    private String[] title = {"预算中心","高级功能","其他设置","账号设置","常见问题","好评鼓励","关于我们"};
+    private int[] images = {R.drawable.setting4,R.drawable.setting2,R.drawable.setting1,R.drawable.setting3,R.drawable.setting5,R.drawable.setting6,R.drawable.setting7};
+
+    private static class SettingItemAdapter extends RecyclerView.Adapter<SettingItemAdapter.MyViewHolder>{
+        private final Context mContext;
+        private final ArrayList<SettingItem> mSettingItems;
+        public SettingItemAdapter(Context context,ArrayList<SettingItem> settingItems){
+            this.mContext = context;
+            this.mSettingItems = settingItems;
+        }
+        public class MyViewHolder extends RecyclerView.ViewHolder{
+            public TextView settingName;
+            public ImageView imageView;
+            public MyViewHolder(View itemView){
+                super(itemView);
+                settingName = (TextView)itemView.findViewById(R.id.setting_name);
+                imageView = (ImageView)itemView.findViewById(R.id.setting_image);
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //可以在这写监听事件
+                    }
+                });
+            }
+        }
+
+        //设置item的监听接口
+        public interface OnItemClickListener{
+            //点击每一项的实现方法
+            public void OnItemClick(View view, SettingItem settingItem);
+        }
+        private OnItemClickListener onItemClickListener;
+
+        public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+            this.onItemClickListener = onItemClickListener;
+        }
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View itemView = View.inflate(mContext,R.layout.setting_item,null);
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            SettingItem data = mSettingItems.get(position);
+            holder.settingName.setText(data.mitemName);
+            holder.imageView.setImageResource(data.mimgId);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mSettingItems.size();
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private ArrayList<SettingItem> initData(){
+        ArrayList<SettingItem> settingItems = new ArrayList<SettingItem>();
+        for(int i = 0; i < 7; i++){
+            SettingItem item = new SettingItem(images[i],title[i]);
+            settingItems.add(item);
+        }
+        return settingItems;
+    }
+    public void initRecyclerView(View view){
+        RecyclerView recyclerView = view.findViewById(R.id.setting_recycleview);
+        SettingItemAdapter settingItemAdapter = new SettingItemAdapter(getActivity(), initData());
+        recyclerView.setAdapter(settingItemAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        settingItemAdapter.setOnItemClickListener(new SettingItemAdapter.OnItemClickListener(){
+            @Override
+            public void OnItemClick(View view, SettingItem settingItem) {
+                //监听事件
+            }
+        });
+    }
+}
