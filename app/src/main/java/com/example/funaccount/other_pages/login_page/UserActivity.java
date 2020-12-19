3bc7ae072b36a27941db1a4a01efa19893c51de3
Jusfan <jiasifan@kuaishou.com>
2020-12-19 11:26:39 +0800

    用ViewModel传递服务器返回的数据

--- app/src/main/java/com/example/funaccount/other_pages/login_page/UserActivity.java
+++ app/src/main/java/com/example/funaccount/other_pages/login_page/UserActivity.java
@@ -2,45 +2,34 @@ package com.example.funaccount.other_pages.login_page;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.funaccount.R;
-import com.example.funaccount.retrofit.GetUserData;
-import com.example.funaccount.retrofit.UserDataReception;
 import com.example.funaccount.setting_page.SettingFragment;
-import com.example.funaccount.util.UserData;
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import com.example.funaccount.util.UserDataViewModel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import static android.content.ContentValues.TAG;
+import androidx.lifecycle.ViewModelProvider;
 
 public class UserActivity extends AppCompatActivity {
-    private Button mReturnButton;
-    private String mUserId = "00000000";
-    private UserData mUserData;
     private TextView mResponseId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user);
-        mReturnButton = (Button)findViewById(R.id.returnback);
-        request();
+        Button mReturnButton = (Button) findViewById(R.id.returnback);
+
+        UserDataViewModel model = new ViewModelProvider(this).get(UserDataViewModel.class);
+        model.getUser(this).observe(this, userData -> {
+            mResponseId = findViewById(R.id.response_id);
+            String text = "id:"+userData.getUserId()+" name:"+userData.getUserName();
+            mResponseId.setText(text);
+        });
     }
     public void backToLogin(View view) {
         Intent intent3 = new Intent(UserActivity.this,LoginActivity.class) ;
@@ -53,43 +42,5 @@ public class UserActivity extends AppCompatActivity {
         SettingFragment settingFragment = new SettingFragment();
         fragmentTransaction.replace(R.id.setting_fragment,settingFragment);
         fragmentTransaction.commit();
-    }
-
-    public void request(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://www.baidu.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) //支持rxjava
-                .build();
-        GetUserData request = retrofit.create(GetUserData.class);
-
-        Observable<UserDataReception> observable = request.getCall(mUserId);
-
-        observable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<UserDataReception>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-                        Log.d(TAG, "开始采用subscribe连接");
-                    }
-
-                    @Override
-                    public void onNext(@NonNull UserDataReception userDataReception) {
-                        mUserData = userDataReception.getUserData();
-                        mResponseId = findViewById(R.id.response_id);
-                        String text = "id:"+mUserData.getUserId()+" name:"+mUserData.getUserName();
-                        mResponseId.setText(text);
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Toast.makeText(getBaseContext(),"发送请求失败",Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.d(TAG,"成功");
-                    }
-                });
     }
 }
---
+++ app/src/main/java/com/example/funaccount/util/UserDataViewModel.java
@@ -0,0 +1,73 @@
+package com.example.funaccount.util;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.funaccount.retrofit.GetUserData;
+import com.example.funaccount.retrofit.UserDataReception;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.content.ContentValues.TAG;
+
+public class UserDataViewModel extends ViewModel {
+    private String mUserId = "00000000";
+    private UserData mUserData;
+    private MutableLiveData<UserData> user;
+
+    public LiveData<UserData> getUser(Context context){
+        if (user == null) {
+            user = new MutableLiveData<UserData>();
+            loadUser(context);
+        }
+        return user;
+    }
+
+    private void loadUser(Context context) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.baidu.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) //支持rxjava
+                .build();
+        GetUserData request = retrofit.create(GetUserData.class);
+
+        Observable<UserDataReception> observable = request.getCall(mUserId);
+
+        observable.subscribeOn(Schedulers.io())     //在io线程进行网络请求
+                .observeOn(AndroidSchedulers.mainThread())      //在主线程处理结果
+                .subscribe(new Observer<UserDataReception>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(@NonNull UserDataReception userDataReception) {
+                        mUserData = userDataReception.getUserData();
+                        user.setValue(mUserData);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Toast.makeText(context, "发送请求失败", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG,"成功");
+                    }
+                });
+    }
+}
